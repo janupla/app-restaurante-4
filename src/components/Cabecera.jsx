88e4a500237aff0d11df8273659999752d3e8f1c
Jana <janagonzalez.f@gmail.com>
2023-05-03 19:21:24 -0400

    cambios varios

---
+++ src/components/Cabecera.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Cabecera.css';
+
+const Cabecera = () => {
+    return (
+        <header className='Cabecera-h1'>
+            <a href="#" className='Cabecera-a'>
+                Sushi Lake
+            </a>
+        </header>
+    );
+};
+
+export default Cabecera;
---
+++ src/components/Reservar.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Reservar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Reservar;
