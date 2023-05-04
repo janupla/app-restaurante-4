5c0d6580dffe0fc86378a909194cc5c962d74088
Jana <janagonzalez.f@gmail.com>
2023-05-03 20:27:40 -0400

    creando inicio

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -3,13 +3,29 @@ import './Cabecera.css';
 
 const Cabecera = () => {
     return (
-        <header className='Cabecera-h1'>
-            
+        <header className='Cabecera'>
+            <h1 className='Cabecera-h1'>
+                <a href="#" className='Cabecera-a'>
+                    Inicio
+                </a>
+            </h1>
 
+            <button className='Cabecera-button'>
+                svg
 
+            </button>
 
+            <nav className='Cabecera-nav'>
+                <ul className='Cabecera-ul'>
+                    <li className="Cabecera-li"><a href="" className="Cabecera-a">Item 1</a></li>
+                    <li className="Cabecera-li"><a href="" className="Cabecera-a">Item 2</a></li>
+                    <li className="Cabecera-li"><a href="" className="Cabecera-a">Item 3</a></li>
+                    <li className="Cabecera-li"><a href="" className="Cabecera-a">Item 4</a></li>
+                    <li className="Cabecera-li"><a href="" className="Cabecera-a">Item 5</a></li>
+                </ul>
+            </nav>
 
-
+         
 
 
         </header>
