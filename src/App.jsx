2d780509cf1d82c1d16e549307983972e360ca14
Jana <janagonzalez.f@gmail.com>
2023-05-06 19:34:31 -0400

    arreglndo errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import './App.css'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import './App.css';
 import Home from './views/Home';
 import Contacto from './views/Contacto';
 import Ubicacion from './views/Ubicacion';
-
-
+import { Form } from './components/Form';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='App'>
-        <Switch>
-          <Route path="/Home" component={Home} />
-          <Route path="/Contacto" component={Contacto} />
-          <Route path="/Ubicacion" component={Ubicacion} />
-
-        </Switch>
+        <Routes>
+          <Route path='/Home' element={<Home />} />
+          <Route path='/Contacto' element={<Contacto />} />
+          <Route path='/Ubicacion' element={<Ubicacion />} />
+          <Route path='/Form' element={<Form />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
 };
+
 export default App;
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,10 +5,13 @@ function Home() {
         <div>
             <h1>Bienvenidos a Aromas Café</h1>
 
-            <p>Hoy, ya consolidados como una Cafetería de Especialidad buscamos preservar y expandir en ese espíritu de buena onda y buen café.
+            <p>
+                Hoy, ya consolidados como una Cafetería de Especialidad buscamos preservar y expandir en ese espíritu de buena onda y buen café.
                 Creemos que una buena cafetería es mucho más que eso. Son los amigos, la conversación y la buena música. Son los planes, las citas,
-                los proyectos y los encuentros. Una gran cafetería crea comunidad, y como en las viejas tertulias,
-                la conversación fluye hasta que ya es tiempo de pedir otro café.</p>
+                los proyectos y los encuentros. Una gran cafetería crea comunidad, y como en las viejas tertulias, la conversación fluye hasta que 
+                ya es tiempo de pedir otro café.
+
+            </p>
         </div>
     );
 }
