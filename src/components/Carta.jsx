a110c7d1d3edf874b32cfd0d5c3377d548568090
Jana <janagonzalez.f@gmail.com>
2023-05-05 20:20:50 -0400

    cambios

--- src/components/Carta.jsx
+++ src/components/Carta.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Carta = () => {
     return (
         <div>
-            <link rel="" href="https://www.flipsnack.com/5DA58EAA9F7/new-flipbook.html" />
+
+            <h1>Carta</h1>
+            {/* <link rel="" href="https://www.flipsnack.com/5DA58EAA9F7/new-flipbook.html" /> */}
+
         </div>
     );
 };
