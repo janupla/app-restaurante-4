16e11d6fe66eca0f002672f9c4a0da1ab1da686a
Jana <janagonzalez.f@gmail.com>
2023-05-06 22:49:29 -0400

    cambios css

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './views/Home';
 import Contacto from './views/Contacto';
 import Ubicacion from './views/Ubicacion';
 import Reserva from './views/Reserva';
-import { Form } from './components/Form';
+import Form from './components/Form';
 
 const App = () => {
   return (
     <div className='App'>
-    
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Contacto' element={<Contacto />} />
-          <Route path='/Ubicacion' element={<Ubicacion />} />
-          <Route path='/Reserva' element={<Reserva />} />
-          <Route path='/Form' element={<Form />} />
-        </Routes>
-    
+      <Home />
+      <Contacto />
+      <Ubicacion />
+      <Reserva />
+
     </div>
   );
 };
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
     return (
-        <div>
+        <div className="home-container">
             <h1>Bienvenidos a Aromas Café</h1>
 
             <p>
                 Hoy, ya consolidados como una Cafetería de Especialidad buscamos preservar y expandir en ese espíritu de buena onda y buen café.
                 Creemos que una buena cafetería es mucho más que eso. Son los amigos, la conversación y la buena música. Son los planes, las citas,
-                los proyectos y los encuentros. Una gran cafetería crea comunidad, y como en las viejas tertulias, la conversación fluye hasta que 
+                los proyectos y los encuentros. Una gran cafetería crea comunidad, y como en las viejas tertulias, la conversación fluye hasta que
                 ya es tiempo de pedir otro café.
 
             </p>
